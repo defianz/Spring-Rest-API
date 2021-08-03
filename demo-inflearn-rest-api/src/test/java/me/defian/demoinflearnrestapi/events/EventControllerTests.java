05917194aaf10f52a4288aa32aac65aad5e53a09
Defian <iautm91@gmail.com>
2021-08-03 11:23:35 +0900

    Add create events test

---
+++ demo-inflearn-rest-api/src/test/java/me/defian/demoinflearnrestapi/events/EventControllerTests.java
@@ -0,0 +1,30 @@
+package me.defian.demoinflearnrestapi.events;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events")
+                .content(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .accept(MediaTypes.HAL_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+}
